c7cbc8d1b5a1d130dbc0cb44aa348470444e30c6
Liam Dennehy <liam@wiemax.net>
2019-08-06 20:03:37 +0200

    execute permission on dep remover & typos
    execute permission on strip-reference-deps.sh

--- tools/strip-reference-deps.sh
+++ tools/strip-reference-deps.sh
@@ -3,5 +3,5 @@
 phpver=$(php --version | head -n1 | cut -d' ' -f2 | cut -d'.' -f1-2)
 if [ "$phpver" == "5.6" ] || [ "$phpver" == "7.0" ] ; then
   echo "Found PHP Version $phpver, stripping incompatible reference implementation dependencies"
-  composer remove --dev nyholm/psr7 nyholm/psr7-server riswallsmith/buzz endframework/zend-httphandlerrunner
+  composer remove --dev --no-update nyholm/psr7 nyholm/psr7-server kriswallsmith/buzz zendframework/zend-httphandlerrunner
   fi
