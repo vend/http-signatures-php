79aeac4b257b176a06e43356deaeed2cff5e1929
Evan Shaw <evan.shaw@lightspeedhq.com>
2022-12-12 08:33:48 +1300

    Fix php-cs-fixer config

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -4,7 +4,7 @@ $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__ . "/src")
     ->in(__DIR__ . "/tests");
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRules([
         '@PSR2' => true,
         '@Symfony' => true,
